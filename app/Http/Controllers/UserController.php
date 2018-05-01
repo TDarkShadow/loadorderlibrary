1f09f1863e93801c5780f00ef0c0785ce3a23bda
Phinocio <github@phinocio.com>
2018-05-01 11:21:03 -0700

    added middleware and method for viewing users mods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Middleware\IsOwner;
 
 class UserController extends Controller
 {
@@ -12,16 +13,8 @@ class UserController extends Controller
 	public function __construct()
 	{
 		$this->middleware("auth", ['except' => ['show']]);
+		$this->middleware(IsOwner::Class, ['except' => ['show']]);
 	}
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index($username, $slug)
-    {
-		echo $username . ' ' .$slug;
-    }
 
     /**
      * Show the form for creating a new resource.
@@ -50,9 +43,23 @@ class UserController extends Controller
      * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show($username)
     {
-		//REEEEEEEEE
+		$loadOrders = \App\User::with(['loadOrder' => function ($query) {
+			$query->where('is_private', false)->orderBy('updated_at', 'desc');
+		}])->where('username', $username)->get();
+
+		foreach ($loadOrders[0]->loadOrder as $loadOrder)
+
+		{
+			//dd($loadOrder->name);
+		}
+
+		//dd($loadOrders[0]->loadOrder[0]->game);
+
+		return view('user.users-mods')
+			->with('loadOrders', $loadOrders)
+			->with('username', $loadOrders[0]->username);
     }
 
     /**
@@ -66,7 +73,6 @@ class UserController extends Controller
 		$user = \App\User::find(\Auth::user()->id);
 		
 		return view('user.edit-user');
-        dd($user);
     }
 
     /**
@@ -97,6 +103,5 @@ class UserController extends Controller
 		$user = \App\User::find(\Auth::user()->id);
 
 		$user->delete();
-        dd($user);
     }
 }
