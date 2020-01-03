9a245f3efc145ef55d5fd2801d2096937f57e0f0
Phinocio <github@phinocio.com>
2020-01-03 13:49:23 -0700

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,10 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('username');
             $table->string('email')->unique();
-			$table->string('password');
-			$table->integer('admin')->default(0);
+            $table->string('password');
+            $table->integer('admin')->default(0);
             $table->rememberToken();
-			$table->timestamp('last_login');
+            $table->timestamp('last_login')->nullable();
             $table->timestamps();
         });
     }
