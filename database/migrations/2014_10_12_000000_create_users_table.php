353f3315f5e456726d11382181b781181d9c21c9
Phinocio <github@phinocio.com>
2020-04-30 14:40:33 -0600

    added admin flag to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+			$table->string('password');
+			$table->smallInteger('is_admin')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
