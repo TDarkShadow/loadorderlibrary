c1069f2d2384cbc352b5626346a3db89f6875861
Phinocio <github@phinocio.com>
2020-04-30 13:37:23 -0600

    move container to layout file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
 	<div class="row justify-content-center">
 
 		<div class="col-md-8">
@@ -24,7 +23,5 @@
 				</div>
 			</div>
 		</div>
-
 	</div>
-</div>
 @endsection
