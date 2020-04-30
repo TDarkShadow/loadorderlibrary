3bf901562aa6235e5b903cfa535dfaa56b8f960d
Phinocio <github@phinocio.com>
2020-04-30 14:04:46 -0600

    add nav item for compare lists

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,9 @@
 						<li>
 							<a class="nav-link" href="/browse">Browse Lists</a>
 						</li>
+						<li class="mr-4">
+							<a class="nav-link" href="/browse">Compare a List</a>
+						</li>
 						<!-- Authentication Links -->
 						@guest
 						<li class="nav-item">
