6008fe69b2cd68db00b62c7f537f16569ab647f9
Phinocio <github@phinocio.com>
2020-04-30 18:20:19 -0600

    added games seeder

---
+++ database/seeds/GamesTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GamesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		DB::table('games')->insert([
+			'name' => 'TESIII Morrowind',
+		]);
+
+		DB::table('games')->insert([
+			'name' => 'TESIV Oblivion',
+		]);
+
+		DB::table('games')->insert([
+			'name' => 'TESV Skyrim LE',
+		]);
+
+		DB::table('games')->insert([
+			'name' => 'TESV Skyrim VR',
+		]);
+
+		DB::table('games')->insert([
+			'name' => 'Fallout 3',
+		]);
+
+		DB::table('games')->insert([
+			'name' => 'Fallout New Vegas',
+		]);
+
+		DB::table('games')->insert([
+			'name' => 'Fallout 4',
+		]);
+
+		DB::table('games')->insert([
+			'name' => 'Fallout 4 VR',
+		]);
+
+		DB::table('games')->insert([
+			'name' => 'Tale of Two Wastelands',
+		]);
+    }
+}
