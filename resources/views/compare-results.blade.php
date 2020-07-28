7f16017c34cb5dc11e60a4be5542f5ab363811f9
Phinocio <github@phinocio.com>
2020-07-27 19:29:11 -0600

    change text

--- resources/views/compare-results.blade.php
+++ resources/views/compare-results.blade.php
@@ -4,7 +4,7 @@
 <div class="row justify-content-center">
 
 	<div class="col-md-12">
-		<h1 class="text-white">Results <small class="text-muted">{{ $list1->name }} VS. {{ $list2->name }}</small></h1> 
+		<h3 class="text-white">{{ $list1->name }} VS. {{ $list2->name }}</h3> 
 		<small class="text-white">Results only show differences between files that exist in both lists and have differences between them.</small>
 		<x-compare-load-orders-results :results=$results :list2=$list2 />
 	</div>
