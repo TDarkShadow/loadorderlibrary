ce5b969e94096f5664eeee6e1f1dd94f941b5d61
Phinocio <github@phinocio.com>
2021-02-03 17:26:14 -0700

    added better errors handling

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -4,30 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Controllers\LoadOrderController;
-use App\Http\Requests\StoreUpload;
+use Illuminate\Support\Facades\Validator;
 
 class UploadController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -35,16 +15,34 @@ class UploadController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreUpload $request)
+    public function store(Request $request)
     {
-		$validated = $request->validated();
+		$validator = Validator::make($request->all(), [
+			'name' => 'required',
+			'description' => 'string|nullable',
+			'game' => 'required',
+			'files' => 'required',
+			'files.*' => 'mimetypes:text/plain,application/x-wine-extension-ini|max:128'
+		], 
+		[
+			'files' => 'Files are required',
+			'files.*.max' => 'Files may not be more than 16KB.',
+			'files.*.mimes' => 'Files must be of type txt or ini'
+		]);
+
+		if ($validator->fails()) {
+			return response()->json($validator->errors(), 400);
+		}
+
+
+
 		$loadOrder = new \App\LoadOrder();
 		$loadOrder->user_id     = auth()->check() ? auth()->user()->id : null;
-		$loadOrder->game_id     = (int) $validated['game'];
-		$loadOrder->slug        = \App\Helpers\CreateSlug::new($validated['name']);
-		$loadOrder->name        = $validated['name'];
-		$loadOrder->description = $validated['description'];
-		$loadOrder->files       = $this->getFileNames((array) $validated['files']);
+		$loadOrder->game_id     = (int) $request->input('game');
+		$loadOrder->slug        = \App\Helpers\CreateSlug::new($request->input('name'));
+		$loadOrder->name        = $request->input('name');
+		$loadOrder->description = $request->input('description');
+		$loadOrder->files       = $this->getFileNames((array) $request->input('files'));
 		$loadOrder->is_private  = $request->input('private') != null;
 		$loadOrder->save();
 
