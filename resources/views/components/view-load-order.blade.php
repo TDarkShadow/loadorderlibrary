4f4da064d9348a2a6811e654b26c0b9f96ada875
Phinocio <github@phinocio.com>
2021-02-17 01:22:47 -0700

    change view of list and add a filter

--- resources/views/components/view-load-order.blade.php
+++ resources/views/components/view-load-order.blade.php
@@ -12,15 +12,15 @@
 			<div class="card-footer text-muted d-flex justify-content-between align-items-center">
 				<small>Uploaded {{ $loadOrder->created_at->diffForHumans() }} by <a href="#">{{ $loadOrder->user->name ?? 'Anonymous' }}</a></small>
 				@if(auth()->check())
-					@if($loadOrder->user == auth()->user() || auth()->user()->is_admin)
-						<span>
-							<form method="POST" action="/lists/{{$loadOrder->slug}}">
-								@method('delete')
-								@csrf
-								<button class="btn text-danger" href="#" role="button">Delete</button>
-							</form>
-						</span>
-					@endif
+				@if($loadOrder->user == auth()->user() || auth()->user()->is_admin)
+				<span>
+					<form method="POST" action="/lists/{{$loadOrder->slug}}">
+						@method('delete')
+						@csrf
+						<button class="btn text-danger" href="#" role="button">Delete</button>
+					</form>
+				</span>
+				@endif
 				@endif
 			</div>
 		</div>
@@ -29,23 +29,78 @@
 	</div>
 </div>
 <div class="row">
-	@foreach($files as $file)
-	<div class="col-md-6">
-		<div class="card text-white bg-dark mb-3">
-			<div class="card-header">
-				<h3>{{ $file['name'] }}</h3>
-			</div>
+	<div class="col-md-12">
+		<div class="accordion" id="accordion">
+			@foreach($files as $file)
+			<div class="card bg-dark col-md-12 mb-1 p-0">
+				<div class="card-header d-flex justify-content-between align-items-center m-0 pl-0" id="heading{{$loop->index}}">
+					<h5 class="mb-0">
+						<button class="btn btn-link collapsed inline" type="button" data-toggle="collapse" data-target="#collapse{{$loop->index}}" aria-expanded="false" aria-controls="collapse{{$loop->index}}">
+							{{ $file['name'] }}
+						</button>
 
-			<table class="table table-striped table-dark">
-				@foreach(explode("\n", $file['content']) as $row)
-				<tr>
-					<td>{{ $row }}</td>
-				</tr>
-				@endforeach
-			</table>
+					</h5>
+					<form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="search" placeholder="Filter..." aria-label="Filter" onkeyup="filter('filter{{$loop->index}}', 'list{{$loop->index}}')" id="filter{{$loop->index}}">
+					</form>
+				</div>
+
+				<div id="collapse{{$loop->index}}" class="collapse" aria-labelledby="heading{{$loop->index}}" data-parent="#accordion">
+					<div class="card-body bg-dark m-0 p-0">
+						<ul class="list-group bg-dark lo-list" id="list{{$loop->index}}">
+							@foreach(explode("\n", $file['content']) as $row)
+							<li class="bg-dark text-white list-group-item lo-list-item d-flex align-items-center">
+								<div class="counter">
+									{{ $loop->index + 1 }}
+								</div>
+								<div class="line">
+
+									{{ $row }}
+								</div>
+							</li>
+							@endforeach
+						</ul>
+						<!-- <table class="table table-striped table-dark">
+							@foreach(explode("\n", $file['content']) as $row)
+							<tr>
+								<td>{{ $row }}</td>
+							</tr>
+							@endforeach
+						</table> -->
+					</div>
+				</div>
+			</div>
+			@endforeach
 
 		</div>
 	</div>
-	@endforeach
 
 </div>
+
+<script>
+	function filter(search, list) {
+		console.log(search, list);
+
+		// Declare variables
+		var input, filter, ul, li, a, i, txtValue;
+		input = document.getElementById(search);
+		filter = input.value.toLowerCase();
+		ul = document.getElementById(list);
+		li = ul.getElementsByTagName('li');
+
+		// Loop through all list items, and hide those who don't match the search query
+
+		for (i = 0; i < li.length; i++) {
+			a = li[i].getElementsByTagName("div")[1];
+			txtValue = a.textContent.trim() || a.innerText.trim();
+			console.log(txtValue.toLowerCase().indexOf(filter));
+			if (txtValue.toLowerCase().indexOf(filter) >= 0) {
+				li[i].style.display = "";
+				li[i].classList = 'bg-dark text-white list-group-item lo-list-item d-flex align-items-center';
+			} else {
+				li[i].style.display = 'none';
+				li[i].classList -= 'd-flex';
+			}
+		}
+	}
+</script>
