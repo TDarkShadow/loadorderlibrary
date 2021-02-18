d181526b82ef2a46f86e71a42f0dac365eabbd9a
Phinocio <github@phinocio.com>
2021-02-18 11:57:40 -0700

    update text on welcome note

--- resources/views/components/guest-welcome.blade.php
+++ resources/views/components/guest-welcome.blade.php
@@ -1,9 +1,9 @@
 <div class="card text-white bg-dark">
-    <div class="card-header">What is this?</div>
+	<div class="card-header">What is this?</div>
 
-    <div class="card-body">
-        <p class="card-text">Load Order Library is a website/tool to share mod lists of Bethesda games with other players. Primarily for debugging purposes, but can be quite useful for YouTuber's to have mod lists for each of their let's play characters, mod list creators to share with users, etc.</p>
+	<div class="card-body">
+		<p class="card-text">Load Order Library is a website/tool to share mod lists of Bethesda games with other players. Primarily for debugging purposes, but can be quite useful for YouTuber's to have mod lists for each of their let's play characters, mod list creators to share with users, etc.</p>
 
-        <p class="card-text">At the moment, we only support Mod Organizer lists, but will expand to support other tools in the near future. To upload a mod list, simply click "upload" and select the requested Mod Organizer files. Once uploaded, a handy link will be provided to share with others! If you would like more features such as editing mod lists, deleting them, among other things, feel free to make an account before uploading. <strong>An account is not required to upload, but recommended for extra features such as editing and deleting lists.</strong></p>
-    </div>
+		<p class="card-text">To upload a mod list, simply click "upload" and select the requested Mod Organizer files. Once uploaded, a handy link will be provided to share with others! <strong>An account is not required to upload, but recommended for extra features such as deleting lists.</strong> More features will come in the future. Checkout the <a href="https://github.com/phinocio/loadorderlibrary/blob/master/README.md">Github README</a> for more info.</p>
+	</div>
 </div>
