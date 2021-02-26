3af2940ccf514af3462a1d08abee1c90f653ffd9
Phinocio <github@phinocio.com>
2021-02-25 17:32:50 -0700

    add admin page with some simple stats

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Middleware\IsAdmin;
+
+class AdminController extends Controller
+{
+	public function __construct()
+	{
+		$this->middleware(IsAdmin::class);
+	}
+
+    public function stats()
+	{
+		$userStats = [];
+		$listStats = [];
+		$fileStats = [];
+
+		$users = \App\User::orderBy('created_at', 'desc')->get();
+		$lists = \App\LoadOrder::all();
+		$files = \Storage::allFiles('uploads');
+
+		
+		$fileSize = 0;
+
+		foreach ($files as $file) {
+			$fileSize += \Storage::size($file);
+		}
+
+		$userStats[] = [
+			"name" => "Users",
+			"value" => count($users)
+		];
+
+		$userStats[] = [
+			"name" => "Admins",
+			"value" => count($users->filter(function ($value, $key) {
+				return $value->is_admin === 1;
+			}))
+		];
+		$userStats[] = [
+			"name" => "Last Registered",
+			"value" => \Carbon\Carbon::createFromTimestamp($users[0]->created_at)->diffForHumans()
+		];
+
+		$listStats[] = [
+			"name" => "Lists",
+			"value" => count($lists)
+		];
+
+		$listStats[] = [
+			"name" => "Anonymous Lists",
+			"value" => count($lists->filter(function ($value, $key) {
+				return $value->user_id == null;
+			}))
+		];
+
+		$listStats[] = [
+			"name" => "Percent Anonymous",
+			"value" => (count($lists->filter(function ($value, $key) {
+				return $value->user_id == null;
+			})) / count($lists)) * 100 . '%'
+		];
+
+		$fileStats[] = [
+			"name" => "Files",
+			"value" => count($files)
+		];
+
+		$fileStats[] = [
+			"name" => "File Size",
+			"value" => $fileSize / 1000000 // Divide by 1 million to get it into MB.
+		];
+
+		return view('admin-stats')->with(['userStats' => $userStats, 'listStats' => $listStats, 'fileStats' => $fileStats]);
+	}
+}
---
+++ resources/views/admin-stats.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('title', 'Change Password')
+
+@section('content')
+<div class="container">
+	<h1 class="text-white">Simple Stats</h1>
+	<div class="row">
+		<div class="d-flex col-md-4 align-items-stretch">
+			<div class="flex-fill card text-white bg-dark">
+				<div class="card-header">
+					<h3 class="card-title p-0 m-0">{{ __('User Stats') }}</h3>
+				</div>
+
+				<div class="card-body p-0">
+					<ul class="list-group bg-dark">
+						@foreach($userStats as $stat)
+						<li class="list-group-item list-group-item-dark d-flex justify-content-between align-items-center">
+							{{ $stat['name'] }}
+							<span class="badge badge-secondary badge-pill">{{ $stat['value'] }}</span>
+						</li>
+						@endforeach
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="d-flex col-md-4 align-items-stretch">
+			<div class="flex-fill card text-white bg-dark">
+				<div class="card-header">
+					<h3 class="card-title p-0 m-0">{{ __('List Stats') }}</h3>
+				</div>
+
+				<div class="card-body p-0">
+					<ul class="list-group bg-dark">
+						@foreach($listStats as $stat)
+						<li class="list-group-item list-group-item-dark d-flex justify-content-between align-items-center">
+							{{ $stat['name'] }}
+							<span class="badge badge-secondary badge-pill">{{ $stat['value'] }}</span>
+						</li>
+						@endforeach
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="d-flex col-md-4 align-items-stretch">
+			<div class="flex-fill card text-white bg-dark">
+				<div class="card-header">
+					<h3 class="card-title p-0 m-0">{{ __('File Stats') }}</h3>
+				</div>
+
+				<div class="card-body p-0">
+					<ul class="list-group bg-dark">
+						<li class="list-group-item list-group-item-dark d-flex justify-content-between align-items-center">
+							{{ $fileStats[0]['name'] }}
+							<span class="badge badge-secondary badge-pill">{{ $fileStats[0]['value'] }}</span>
+						</li>
+
+						<li class="list-group-item list-group-item-dark d-flex justify-content-between align-items-center">
+							{{ $fileStats[1]['name'] }}
+							<span class="badge {{ ($fileStats[1]['value'] > 10) ? 'badge-danger' : 'badge-secondary' }} badge-pill">{{ $fileStats[1]['value'] }} MB</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
