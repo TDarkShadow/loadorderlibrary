964ecf75c193efa16b97afb40fdfe5c8bca4f9cf
Phinocio <github@phinocio.com>
2021-03-01 23:26:30 -0700

    added badge to indicate quickly if theres changes in a file. Fixed saying files instead of lines

--- resources/views/components/compare-load-orders-results.blade.php
+++ resources/views/components/compare-load-orders-results.blade.php
@@ -7,18 +7,20 @@
 					<span class="text-white">&#11166;</span> <b>{{ $file['filename'] }}</b>
 				</button>
 			</h5>
+
+			<span class="badge badge-pill {{ $file['class'] }}">{{ $file['class'] == 'badge-danger' ? 'Differences' : 'No Differences' }}</span>
 		</div>
 
 		<div id="collapse{{$loop->index}}" class="collapse" aria-labelledby="heading{{$loop->index}}" data-parent="#accordion">
 			<div class="text-white card-body bg-dark m-0 pr-10">
 				<div class="files-heading d-flex justify-content-between align-self-stretch mb-2">
 					<div class="missing">
-						<h3 class="text-white">Missing Files</h3>
+						<h3 class="text-white">Missing Lines</h3>
 						<small class="text-white">Mods/settings not in <b>{{ $list1->name }}</b></small>
 					</div>
 
 					<div class="added">
-						<h3 class="text-white">Added Files</h3>
+						<h3 class="text-white">Added Lines</h3>
 						<small class="text-white">Mods/settings not in <b>{{ $list2->name }}</b></small>
 					</div>
 				</div>
@@ -65,29 +67,3 @@
 	</div>
 	@endforeach
 </div>
-
-<!-- <script>
-	function filter(search, list) {
-
-		// Declare variables
-		var input, filter, ul, li, a, i, txtValue;
-		input = document.getElementById(search);
-		filter = input.value.toLowerCase();
-		ul = document.getElementById(list);
-		li = ul.getElementsByTagName('li');
-
-		// Loop through all list items, and hide those who don't match the search query
-		for (i = 0; i < li.length; i++) {
-
-			a = li[i].getElementsByTagName("div")[0];
-			txtValue = a.textContent.trim() || a.innerText.trim();
-			if (txtValue.toLowerCase().indexOf(filter) >= 0) {
-				li[i].style.display = "";
-				li[i].classList = 'bg-dark text-white list-group-item lo-list-item d-flex align-items-center';
-			} else {
-				li[i].style.display = 'none';
-				li[i].classList -= 'd-flex';
-			}
-		}
-	}
-</script> -->
