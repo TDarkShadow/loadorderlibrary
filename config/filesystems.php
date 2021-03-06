6cefa35180c57eb96db7a1f57cd012254ede5c18
Phinocio <github@phinocio.com>
2021-03-05 22:00:39 -0700

    add tmp disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,11 @@ return [
             'root' => storage_path('app/uploads')
         ],
 
+		'tmp' => [
+			'driver' => 'local',
+			'root' => storage_path('app/tmp')
+		],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
