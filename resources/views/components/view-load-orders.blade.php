2c7ed0d4c0227b366955edb60674ec3bf9e7ed9b
Phinocio <github@phinocio.com>
2021-03-05 20:02:39 -0700

    change wording of delete list button

--- resources/views/components/view-load-orders.blade.php
+++ resources/views/components/view-load-orders.blade.php
@@ -32,7 +32,7 @@
 					<form method="POST" action="/lists/{{$loadOrder->slug}}">
 						@method('delete')
 						@csrf
-						<button class="btn text-danger" href="#" role="button">Delete</button>
+						<button class="btn text-danger" href="#" role="button">Delete List</button>
 					</form>
 				</span>
 				@endif
