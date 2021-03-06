c3367ba8af635ac4d07d697f6b70093c2af45b07
Phinocio <github@phinocio.com>
2021-03-06 16:57:13 -0700

    add list of valid files to upload page

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
 	<div class="row justify-content-center">
 
-		<div class="col-md-6">
+		<div class="col-md-8 card-group">
 			<div class="card text-white bg-dark">
 				<div class="card-header text-center">
 					Upload Files
@@ -14,6 +14,23 @@
 
 				<div class="card-body">
 					<x-upload-form :games=$games />
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 card-group">
+			<div class="card text-white bg-dark">
+				<div class="card-header text-center">
+					Valid Files
+				</div>
+
+				<div class="card-body p-0">
+					<ul class="list-group bg-dark">
+						@foreach($validFiles as $file)
+						<li class="bg-dark text-white list-group-item list-group-item-dark d-flex py-1">
+							{{$file}}
+						</li>
+						@endforeach
+					</ul>
 				</div>
 			</div>
 		</div>
