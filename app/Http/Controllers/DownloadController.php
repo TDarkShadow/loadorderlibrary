9bb7ed5dcc608bb0b8c7328662de3f83789819ca
Phinocio <github@phinocio.com>
2021-03-05 21:29:02 -0700

    create download controller

---
+++ app/Http/Controllers/DownloadController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use ZipArchive;
+
+class DownloadController extends Controller
+{
+    public function index(\App\LoadOrder $loadOrder, $fileName)
+	{
+		$listFiles = explode(',', $loadOrder->files);
+
+		// DL all files.
+		if($fileName == 'all') {
+			$zip = new ZipArchive;
+			$zipFile = $loadOrder->name . '.zip';
+			if($zip->open(storage_path('app/tmp/' . $zipFile), ZipArchive::CREATE)) {
+				foreach($listFiles as $file) {
+					$zip->addFile(storage_path('app/uploads/' . $file), preg_replace('/[a-zA-Z0-9_]*-/i', '', $file));
+				}
+				$zip->close();
+				
+				return \Storage::download('tmp/' . $zipFile);
+			}
+		}
+
+		$file = implode(preg_grep("/$fileName/", $listFiles));
+
+		return \Storage::download('uploads/' . $file, $fileName);
+
+	}
+}
