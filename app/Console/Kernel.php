cd449719a9b3ec5bc67d349dcdf02c0b9a1b082d
Phinocio <github@phinocio.com>
2021-03-07 19:25:28 -0700

    make schedule every minute to test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-		$schedule->command('tmp:clear')->daily();
+		$schedule->command('tmp:clear')->everyMinute();
     }
 
     /**
