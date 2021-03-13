4f4ab13e86b69beed3e872f0b4da93448215a379
Phinocio <github@phinocio.com>
2021-03-13 14:42:22 -0700

    update form updates

--- resources/views/components/edit-load-order-form.blade.php
+++ resources/views/components/edit-load-order-form.blade.php
@@ -1,8 +1,9 @@
-<form method="POST" enctype="multipart/form-data">
+<form method="POST" action="{{ route('lists.update', $loadOrder->slug) }}" enctype="multipart/form-data">
+	@method('PUT')
 	@csrf
 	<div class="form-group">
 		<label for="name">List Name</label>
-		<input name="name" type="text" class="form-control @error('name') is-invalid @enderror" id=" name" value="{{ old('name') }}">
+		<input name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name" value="{{ old('name') ?? $loadOrder->name }}">
 		@error('name')
 		<span class="invalid-feedback" role="alert">
 			<strong>{{ $message }}</strong>
@@ -12,7 +13,7 @@
 
 	<div class="form-group">
 		<label for="description">Description</label>
-		<textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description" rows="3" value="{{ old('description') }}">{{ $loadOrder->description }}</textarea>
+		<textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description" rows="3">{{ old('description') ?? $loadOrder->description }}</textarea>
 		@error('description')
 		<span class="invalid-feedback" role="alert">
 			<strong>{{ $message }}</strong>
@@ -25,7 +26,7 @@
 		<select name="game" class="form-control @error('game') is-invalid @enderror" id="game">
 			<option value="">-Choose Game-</option>
 			@foreach($games as $game)
-			<option value={{ $game->id }} @if(old('game')==$game->id) selected @endif> {{ $game->name }}</option>
+			<option {{ $game->id == $loadOrder->game->id ? 'selected' : '' }} value={{ $game->id }} @if(old('game')==$game->id) selected @endif> {{ $game->name }}</option>
 			@endforeach
 		</select>
 		@error('game')
@@ -36,7 +37,7 @@
 	</div>
 
 	<div class="form-check form-check-inline">
-		<input name="private" class="form-check-input @error('private') is-invalid @enderror" type="checkbox" value="private" id="private" aria-describedby="privateHelp">
+		<input name="private" class="form-check-input @error('private') is-invalid @enderror" type="checkbox" value="private" id="private" aria-describedby="privateHelp" {{ $loadOrder->is_private ? 'checked' : ''}}>
 		<label class="form-check-label" for="private">
 			Private?
 		</label>
@@ -51,14 +52,27 @@
 	</small>
 
 	<div class="form-group">
-		<label for="files">Choose Files</label>
-		<input name="files[]" type="file" class="form-control-file @error('files.*') is-invalid @enderror" id="files" accept=".txt,.ini" multiple required>
-		<span class="text-muted">It's recommended to copy the files you want to upload to a single folder, then choose them all from that folder.</span>
+		<label for="files">Add/Edit Files</label>
+		<input name="files[]" type="file" class="form-control-file @error('files.*') is-invalid @enderror" id="files" accept=".txt,.ini" multiple>
+		<span class="text-muted">It's recommended to copy the files you want to upload to a single folder, then choose them all from that folder. Files uploaded of the same name as one that's already in the list will replace it. If you are not adding/updating files, don't select any.</span>
 		@error('files.*')
 		<span class="invalid-feedback" role="alert">
 			<strong>{{ $message }}</strong>
 		</span>
 		@enderror
 	</div>
-	<button class="btn btn-primary" type="submit">Upload</button>
+
+	<div class="form-group">
+		<label>Files In List</label>
+		<span class="text-muted">Uncheck a file to remove it from the list</span>
+		@foreach($loadOrder->files as $file)
+		<div class="form-check">
+			<input name="existing-files[]" class="form-check-input" type="checkbox" value="{{ $file->clean_name . '-' . $file->id }}" id="check-{{ $file->clean_name }}" checked>
+			<label class="form-check-label" for="check-{{ $file->clean_name }}">
+				{{ $file->clean_name }}
+			</label>
+		</div>
+		@endforeach
+	</div>
+	<button class="btn btn-primary" type="submit">Update List</button>
 </form>
--- resources/views/edit-load-order.blade.php
+++ resources/views/edit-load-order.blade.php
@@ -5,9 +5,36 @@
 @section('image', url('/images/' . $loadOrder->game->name . '.png'))
 
 @section('content')
-<div class="row justify-content-center">
-	<div class="col-md-12">
-		<x-edit-load-order :loadOrder=$loadOrder />
+<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-8 card-group">
+			<div class="card text-white bg-dark">
+				<div class="card-header text-center">
+					Edit List
+				</div>
+
+				<div class="card-body">
+					<x-edit-load-order-form :loadOrder=$loadOrder :games=$games />
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 card-group">
+			<div class="card text-white bg-dark">
+				<div class="card-header text-center">
+					Valid Files
+				</div>
+
+				<div class="card-body p-0">
+					<ul class="list-group bg-dark">
+						@foreach($validFiles as $file)
+						<li class="bg-dark text-white list-group-item list-group-item-dark d-flex py-1">
+							{{$file}}
+						</li>
+						@endforeach
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 @endsection
