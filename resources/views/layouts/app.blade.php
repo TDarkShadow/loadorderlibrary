153f0f1d6de197c6df1ea85b874273c81b06c476
Phinocio <github@phinocio.com>
2021-03-15 18:08:47 -0600

    change order of title name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<title>{{ config('app.name', 'Load Order Library') }} - @yield('title')</title>
+	<title>@yield('title') - {{ config('app.name', 'Load Order Library') }}</title>
 
 	<!-- Fonts -->
 	<!-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
