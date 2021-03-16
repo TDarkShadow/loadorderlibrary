bdd5d836505acb84203a84b2491fdb25a3e046d8
Phinocio <github@phinocio.com>
2021-03-15 18:42:00 -0600

    added custom rule to check for mimetypes

--- app/Http/Requests/StoreUpload.php
+++ app/Http/Requests/StoreUpload.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Rules\ValidFilename;
+use App\Rules\ValidMimetype;
 use App\Rules\ValidNumLines;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -32,7 +33,7 @@ class StoreUpload extends FormRequest
             'description' => 'string|nullable',
             'game' => 'required',
             'files' => 'required',
-            'files.*' => ['mimetypes:text/plain,application/x-wine-extension-ini', 'max:128', new ValidNumLines, new ValidFilename]
+            'files.*' => [new ValidMimetype, 'max:128', new ValidNumLines, new ValidFilename]
         ];
     }
 
