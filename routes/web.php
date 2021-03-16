bef66b64da5bdee31d3b6e9fe68d48399ca39d13
Phinocio <github@phinocio.com>
2021-03-15 20:16:20 -0600

    add games and author routes

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::get('/lists/{load_order:slug}', 'LoadOrderController@show');
 Route::get('/lists/{load_order:slug}/download/{file}', 'DownloadController@index');
 Route::delete('/lists/{load_order:slug}', 'LoadOrderController@destroy');
 
+// List View By Game/Author Routes.
+Route::get('/game/{game:name}', 'LoadOrderController@showByGame')->name('lists.show-by-game');
+Route::get('/author/{user:name}', 'LoadOrderController@showByAuthor')->name('lists.show-by-author');
+
 // Comparison Routes.
 Route::get('/compare', 'ComparisonController@index')->name('compare');
 Route::post('/compare', 'ComparisonController@post')->name('compare-post');
@@ -36,7 +40,6 @@ Route::get('/account/change-password', 'ChangePasswordController@index')->name('
 Route::post('/account/change-password', 'ChangePasswordController@store')->name('change-password-post');
 Route::get('/account/delete', 'DeleteAccountController@index')->name('delete-account');
 Route::post('/account/delete', 'DeleteAccountController@destroy')->name('delete-account-post');
-// TODO: deleted/edit. 
 
 // Admin routes
 Route::get('admin/stats', 'AdminController@stats')->name('admin-stats');
