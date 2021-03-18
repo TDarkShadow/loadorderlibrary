9254dcc912acc199f5af4999e19a005cb5546158
Phinocio <github@phinocio.com>
2021-03-18 13:46:07 -0600

    added destroy method to usercontroller to delete accounts

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,4 +10,16 @@ class UserController extends Controller
     public function index() {
 		return view('user.profile');
 	}
+
+	public function destroy() {
+		$name = auth()->user()->name;
+		try {
+			flash('Account <b>' . $name . '</b> and all its lists successfully deleted!')->success()->important();
+			auth()->user()->delete();
+			return redirect('/');
+		} catch (\Throwable $th) {
+			flash('Something went wrong with account deletion. Please <a href="https://github.com/phinocio/loadorderlibrary/issues/new">make a Github issue</a> and let Phin know.')->error()->important();
+			return redirect()->back();
+		}
+	}
 }
