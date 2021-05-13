ce2ddefa3789a10c2815d307e1a5143cb0a7e5a2
Phinocio <github@phinocio.com>
2021-05-13 17:15:01 -0600

    add support me route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/support-me', function () {
+	return view('support-me');
+});
 
 // List View/Upload Routes.
 Route::get('/lists', 'LoadOrderController@index')->name('lists');
