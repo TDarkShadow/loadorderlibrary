9e891c54d9770f1b57dca0d9490f29501befa0aa
Phinocio <github@phinocio.com>
2021-08-19 12:49:32 -0600

    rename backup to backups

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::post('/user/delete', 'UserController@destroy')->name('user.delete-account
 Route::get('admin/stats', 'AdminController@stats')->name('admin.stats');
 Route::get('admin/users', 'AdminController@users')->name('admin.users');
 Route::post('admin/users/verify/{user:id}', 'AdminController@verify')->name('admin.users.verify');
-Route::get('/admin/backup', 'AdminController@backup')->name('admin.backup');
+Route::get('/admin/backups', 'AdminController@backups')->name('admin.backup');
 
 
 // Intentional error routes for testing purposes.
